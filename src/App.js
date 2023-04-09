a140161ec09ce1605884395c2fc3459a98276f09
phillitron3000 <cphilleo@gmail.com>
2023-04-09 10:18:25 -0500

    add artist field and submit button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './styles.css';
 function App() {
   return (
     <div className="wrapper">
-      <h1>Record Collecsh</h1>
+      <h1>Record not.</h1>
+      <form>
+        <fieldset>
+          <label>
+            <p>Artist</p>
+            <input name="artist" />
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
