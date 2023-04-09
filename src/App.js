961b909744467d8e113dd852df3868b41668d22f
phillitron3000 <cphilleo@gmail.com>
2023-04-09 10:51:43 -0500

    setFormData and add new fields

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import './styles.css';
 
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+}
+
 function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
   const [submitting, setSubmitting] = useState(false);
   const handleSubmit = event => {
     event.preventDefault();
@@ -11,18 +19,49 @@ function App() {
       setSubmitting(false);
     }, 3000)
   };
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
   
   return (
     <div className="wrapper">
       <h1>Record not.</h1>
       {submitting && 
-        <div>Submitting form.... </div>
+        <div>Submitting form.... 
+          <ul>
+           {Object.entries(formData).map(([name, value]) => (
+             <li key={name}><strong>{name}</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+        </div>
       }
       <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
             <p>Artist</p>
-            <input name="artist" />
+            <input name="artist" onChange={handleChange} />
+          </label>
+        </fieldset>
+        <fieldset>
+          <label>
+            <p>Album Title</p>
+            <input name="title" onChange={handleChange} />
+          </label>
+        </fieldset>
+        <fieldset>
+          <label>
+            <p>Label</p>
+            <input name="label" onChange={handleChange} />
+          </label>
+        </fieldset>
+        <fieldset>
+          <label>
+            <p>Year</p>
+            <input name="year" onChange={handleChange} />
           </label>
         </fieldset>
         <button type="submit">Submit</button>
